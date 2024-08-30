aefb2c9578daf3b3c1a3708a035d061f7fee2463
git-Sjd <sajid.tisl@gmail.com>
2024-08-31 01:29:34 +0530

    weatherApp

---
+++ src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div className='text-blue-500 text-7xl my-24'>
+      this is second page of App
+    </div>
+  )
+}
+
+export default Weather;
